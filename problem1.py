fb1657ffa023086a6d2265e717b2d310b1e5f939
Jinank <jinankthakker@gmail.com>
2025-03-07 15:55:44 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -4,25 +4,11 @@ import streamlit as st
 
 file_path = 'usa_air_traffic.csv'  # Adjust the file path based on where your file is stored
 flight_data = pd.read_csv(file_path)
-print(flight_data.head())
-
-
-st.title("First")
 df = pd.DataFrame(flight_data)
 
 # Display dataset
-st.write("First")
 st.write(df)
 
-
-# Sample dataset of direct flights from JFK
-flight_data = {
-    'Destination': ['LAX', 'LHR', 'CDG', 'MIA', 'SFO'],
-    'Airline': ['American Airlines', 'Delta Air Lines', 'Air France', 'JetBlue Airways', 'United Airlines'],
-    'Frequency': [120, 90, 70, 100, 80]
-}
-
-st.title("JFK Flight Routes Analysis")
 df = pd.DataFrame(flight_data)
 
 # Display dataset
