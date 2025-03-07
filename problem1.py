a93b911e97418169feaa014dde0e15e74e5e130a
Jinank <jinankthakker@gmail.com>
2025-03-07 16:00:13 -0500

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-file_path = 'https://github.com/plotly/datasets/blob/master/2011_february_us_airport_traffic.csv'  # Adjust the file path based on where your file is stored
+file_path = 'flightdata.csv'  # Adjust the file path based on where your file is stored
 flight_data = pd.read_csv(file_path)
 df = pd.DataFrame(flight_data)
 
@@ -17,7 +17,7 @@ st.write(df)
 
 
 # Create a bar chart
-fig = px.bar(df, x="Destination", y="Airline", title="Direct Flights from JFK:")
+fig = px.bar(df, x="dest", y="airline", title="Direct Flights from JFK:")
 
 # Display the Plotly chart in Streamlit
 st.plotly_chart(fig)    
